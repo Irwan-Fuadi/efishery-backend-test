0fce73cad94f31496c2b809351d729e44773ecb4
Irwan Fuadi <59-irwanfuadi_@users.noreply.allyoucaneat.mncnow.id>
2021-12-31 06:57:12 +0700

    fix(utils): Fix file and token utils

--- src/utils/file.js
+++ src/utils/file.js
@@ -5,47 +5,45 @@ const FILENAME = process.env.DBFILE_NAME || 'db.json'
 const DIRFILE = '../../data/'
 const FILE = `${DIRFILE}${FILENAME}`
 
-const saveData = async(table, payload) => {
-    const JSONFILE = path.join(__dirname, FILE)
 
-    fs.readFile(JSONFILE, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            data = JSON.parse(data)
-            data[table].push(payload)
+module.exports = {
+    saveData: async(table, payload) => {
+        const JSONFILE = path.join(__dirname, FILE)
 
-            var jsonPayload = JSON.stringify(data, null, 2)
-            fs.writeFile(JSONFILE, jsonPayload, 'utf8', (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return
+        fs.readFile(JSONFILE, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                data = JSON.parse(data)
+                data[table].push(payload)
+
+                var jsonPayload = JSON.stringify(data, null, 2)
+                fs.writeFile(JSONFILE, jsonPayload, 'utf8', (err, data) => {
+                    if (err) {
+                        console.error(err);
+                        return
+                    }
+                })
+            }
+        })
+
+        return payload
+    },
+
+    findData: async(table, field, key) => {
+        const JSONFILE = path.join(__dirname, FILE)
+
+        var data = await JSON.parse(fs.readFileSync(JSONFILE, 'utf8'))
+
+        var result = null
+        if (data) {
+            data[table].forEach((item) => {
+                if (item[field] == key) {
+                    result = item
                 }
             })
         }
-    })
 
-    return payload
-}
-
-const findData = async(table, field, key) => {
-    const JSONFILE = path.join(__dirname, FILE)
-
-    var data = await JSON.parse(fs.readFileSync(JSONFILE, 'utf8'))
-
-    var result = null
-    if (data) {
-        data[table].forEach((item) => {
-            if (item[field] == key) {
-                result = item
-            }
-        })
+        return result
     }
-
-    return result
-}
-
-module.exports = {
-    saveData,
-    findData
 }
